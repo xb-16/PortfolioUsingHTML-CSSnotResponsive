1d4b9e65cf398622c63955374dca431e025be508
XB-16 <xb-16@localhost.localdomain>
2021-02-06 18:28:09 +0100

    add files

---
+++ script.js
@@ -0,0 +1,15 @@
+function change1() {
+    var changed1 = document.getElementById("a"),changed2 = document.getElementById("b");
+    changed1.innerHTML = "W3Schools is optimized for learning, testing, and training. Examples might be simplified to improve reading and basic understanding,  W3Schools is optimized for learning, testing, and training. Examples might be simplified to improve reading and basic understanding.";
+    changed2.innerHTML="John Stone";
+}
+function change2() {
+    var changed1 = document.getElementById("a"),changed2 = document.getElementById("b");
+    changed1.innerHTML = "you agree to have read and accepted our terms of use, cookie and privacy policy. Copyright 1999-2020 by Refsnes Data. All Rights Reserved.Powered by W3.CSS.";
+    changed2.innerHTML="Jakoub Moulay";
+}
+function change3() {
+    var changed1 = document.getElementById("a"),changed2 = document.getElementById("b");
+    changed1.innerHTML = "W3Schools is optimized for learning, testing, and training. Examples might be simplified to improve reading and basic understanding.";
+    changed2.innerHTML="Alex Paul";
+}
